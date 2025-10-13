c9256340c80e8b4ec7cedc143f6181b7402b188d
JuanDProg123 <150536311+JuanDProg123@users.noreply.github.com>
2025-10-12 21:17:02 -0500

    Upload inicial

---
+++ proyectoFinalISW2/proyectoFinalISW2/src/test/java/com/proyectoFinalISW2/proyectoFinalISW2/ProyectoFinalIsw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoFinalISW2.proyectoFinalISW2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalIsw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
